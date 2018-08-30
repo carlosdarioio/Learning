3c7b677ca8d2aee2db29e723361c89c0a67ff7a7
Carlos Dario Flores <cdfn3@hotmail.com>
2018-08-29 20:38:28 -0600

    Add files via upload

---
+++ Normas.php
@@ -0,0 +1,276 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle frontpage.
+ *
+ * @package    core
+ * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+if (!file_exists('./config.php')) {
+    header('Location: install.php');
+    die;
+}
+
+require_once('config.php');
+require_once($CFG->dirroot .'/course/lib.php');
+require_once($CFG->libdir .'/filelib.php');
+
+redirect_if_major_upgrade_required();
+
+$urlparams = array();
+if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_MY) && optional_param('redirect', 1, PARAM_BOOL) === 0) {
+    $urlparams['redirect'] = 0;
+}
+$PAGE->set_url('/', $urlparams);
+$PAGE->set_course($SITE);
+$PAGE->set_other_editing_capability('moodle/course:update');
+$PAGE->set_other_editing_capability('moodle/course:manageactivities');
+$PAGE->set_other_editing_capability('moodle/course:activityvisibility');
+
+// Prevent caching of this page to stop confusion when changing page after making AJAX changes.
+$PAGE->set_cacheable(false);
+
+if ($CFG->forcelogin) {
+    require_login();
+} else {
+    user_accesstime_log();
+}
+
+$hassiteconfig = has_capability('moodle/site:config', context_system::instance());
+
+// If the site is currently under maintenance, then print a message.
+if (!empty($CFG->maintenance_enabled) and !$hassiteconfig) {
+    print_maintenance_message();
+}
+
+if ($hassiteconfig && moodle_needs_upgrading()) {
+    redirect($CFG->wwwroot .'/'. $CFG->admin .'/index.php');
+}
+
+if (get_home_page() != HOMEPAGE_SITE) {
+    // Redirect logged-in users to My Moodle overview if required.
+    $redirect = optional_param('redirect', 1, PARAM_BOOL);
+    if (optional_param('setdefaulthome', false, PARAM_BOOL)) {
+        set_user_preference('user_home_page_preference', HOMEPAGE_SITE);
+    } else if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_MY) && $redirect === 1) {
+        redirect($CFG->wwwroot .'/my/');
+    } else if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_USER)) {
+        $frontpagenode = $PAGE->settingsnav->find('frontpage', null);
+        if ($frontpagenode) {
+            $frontpagenode->add(
+                get_string('makethismyhome'),
+                new moodle_url('/', array('setdefaulthome' => true)),
+                navigation_node::TYPE_SETTING);
+        } else {
+            $frontpagenode = $PAGE->settingsnav->add(get_string('frontpagesettings'), null, navigation_node::TYPE_SETTING, null);
+            $frontpagenode->force_open();
+            $frontpagenode->add(get_string('makethismyhome'),
+                new moodle_url('/', array('setdefaulthome' => true)),
+                navigation_node::TYPE_SETTING);
+        }
+    }
+}
+
+// Trigger event.
+course_view(context_course::instance(SITEID));
+
+// If the hub plugin is installed then we let it take over the homepage here.
+if (file_exists($CFG->dirroot.'/local/hub/lib.php') and get_config('local_hub', 'hubenabled')) {
+    require_once($CFG->dirroot.'/local/hub/lib.php');
+    $hub = new local_hub();
+    $continue = $hub->display_homepage();
+    // Function display_homepage() returns true if the hub home page is not displayed
+    // ...mostly when search form is not displayed for not logged users.
+    if (empty($continue)) {
+        exit;
+    }
+}
+
+$PAGE->set_pagetype('site-index');
+$PAGE->set_docs_path('');
+$PAGE->set_pagelayout('frontpage');
+$editing = $PAGE->user_is_editing();
+$PAGE->set_title($SITE->fullname);
+$PAGE->set_heading($SITE->fullname);
+$courserenderer = $PAGE->get_renderer('core', 'course');
+echo $OUTPUT->header();
+
+// Print Section or custom info.
+$siteformatoptions = course_get_format($SITE)->get_format_options();
+$modinfo = get_fast_modinfo($SITE);
+$modnames = get_module_types_names();
+$modnamesplural = get_module_types_names(true);
+$modnamesused = $modinfo->get_used_module_names();
+$mods = $modinfo->get_cms();
+echo '
+<!-- 
+
+
+ 
+  <link rel="stylesheet" href="css/flexslider.css"/>
+  <link rel="stylesheet" href="css/sl-slide.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  -->
+    <link rel="stylesheet" href="css/mainn.css">
+<style>
+ul.faq li span.number{
+	display:block;
+	float:left;
+	width:50px;
+	height:50px;
+	line-height:50px;
+	text-align:center;
+	background:#57C8E7;
+	color:#fff;
+	font-size:24px;
+	border-radius:50%}
+
+</style>	
+   
+    <section class="title">
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span4">
+                    <h1>Normas</h1>
+                </div>
+                </div>
+        </div>
+    </section>
+    <!-- / .title -->       
+
+    <section id="faqs" class="container">
+	 <div class="row-fluid">
+                <div class="span9">
+          
+        <ul class="faq">
+         <li>
+            <span class="number">01</span>
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            </div>
+        </li>
+
+        <li>
+            <span class="number">02</span>
+            <div>
+                <h4>consectetuer ipsum dolor sit amet</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi</p>
+            </div>
+        </li>
+
+        <li>
+            <span class="number">03</span>
+            <div>
+                <h4>adipiscing ipsum dolor sit amet, consectetuer</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis.</p>
+            </div>
+        </li>
+
+        <li>
+            <span class="number">04</span>
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            </div>
+        </li>
+
+        <li>
+            <span class="number">05</span>
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            </div>
+        </li>
+
+        <li>
+            <span class="number">06</span>
+            <div>
+                <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            </div>
+        </li>
+    </ul>
+    <p>&nbsp;</p>
+    </div></div>
+</section>';
+if (!empty($CFG->customfrontpageinclude)) {
+    include($CFG->customfrontpageinclude);
+
+} else if ($siteformatoptions['numsections'] > 0) {
+    if ($editing) {
+        // Make sure section with number 1 exists.
+        course_create_sections_if_missing($SITE, 1);
+        // Re-request modinfo in case section was created.
+        $modinfo = get_fast_modinfo($SITE);
+    }
+    $section = $modinfo->get_section_info(1);
+    if (($section && (!empty($modinfo->sections[1]) or !empty($section->summary))) or $editing) {
+        echo $OUTPUT->box_start('generalbox sitetopic');
+
+        // If currently moving a file then show the current clipboard.
+        if (ismoving($SITE->id)) {
+            $stractivityclipboard = strip_tags(get_string('activityclipboard', '', $USER->activitycopyname));
+            echo '<p><font size="2">';
+            echo "$stractivityclipboard&nbsp;&nbsp;(<a href=\"course/mod.php?cancelcopy=true&amp;sesskey=".sesskey()."\">";
+            echo get_string('cancel') . '</a>)';
+            echo '</font></p>';
+        }
+
+        $context = context_course::instance(SITEID);
+
+        // If the section name is set we show it.
+        if (!is_null($section->name)) {
+            echo $OUTPUT->heading(
+                format_string($section->name, true, array('context' => $context)),
+                2,
+                'sectionname'
+            );
+        }
+
+        $summarytext = file_rewrite_pluginfile_urls($section->summary,
+            'pluginfile.php',
+            $context->id,
+            'course',
+            'section',
+            $section->id);
+        $summaryformatoptions = new stdClass();
+        $summaryformatoptions->noclean = true;
+        $summaryformatoptions->overflowdiv = true;
+
+        echo format_text($summarytext, $section->summaryformat, $summaryformatoptions);
+
+        if ($editing && has_capability('moodle/course:update', $context)) {
+            $streditsummary = get_string('editsummary');
+            echo "<a title=\"$streditsummary\" ".
+                 " href=\"course/editsection.php?id=$section->id\"><img src=\"" . $OUTPUT->pix_url('t/edit') . "\" ".
+                 " class=\"iconsmall\" alt=\"$streditsummary\" /></a><br /><br />";
+        }
+
+        $courserenderer = $PAGE->get_renderer('core', 'course');
+        echo $courserenderer->course_section_cm_list($SITE, $section);
+
+        echo $courserenderer->course_section_add_cm_control($SITE, $section->section);
+        echo $OUTPUT->box_end();
+    }
+}
+// Include course AJAX.
+
+echo $OUTPUT->footer();
+
